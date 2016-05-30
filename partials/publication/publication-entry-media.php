e46f7ff1e010cc674ae373a5a24412a943588c87
sptheme <sopheak.peas@gmail.com>
2016-05-30 15:55:54 +0700

    Removed link from publication post media

--- partials/publication/publication-entry-media.php
+++ partials/publication/publication-entry-media.php
@@ -15,12 +15,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 $img_size = 'thumb-portrait'; ?>
 
 <div class="publication-entry-media entry-media">
-	<a href="<?php the_permalink(); ?>" title="<?php wpsp_esc_title(); ?>" rel="bookmark" class="related-post-thumb">
-		<?php wpsp_post_thumbnail_total( array(
-				'size'   => $img_size,
-				'width'  => '90',
-				'height' => '140',
-				'alt'    => wpsp_get_esc_title(),
-			) ); ?>
-	</a>
+	
+	<?php wpsp_post_thumbnail_total( array(
+			'size'   => $img_size,
+			'width'  => '90',
+			'height' => '140',
+			'alt'    => wpsp_get_esc_title(),
+		) ); ?>
+
 </div> <!-- #post-media -->
